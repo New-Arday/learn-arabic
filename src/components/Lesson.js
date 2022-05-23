67309bdf3685e00cc3f850b8a8d5df7b80ff56e5
Safia <safia.ibrahim2121@gmail.com>
2022-05-23 19:09:25 +0100

    Applied css styles on lesson page

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -7,6 +7,7 @@ import "./lesson.css";
 function Lesson({ setLessons, lessons }) {
   const [lesson, setLesson] = useState(null);
   const [index, setIndex] = useState(0);
+  const [finished, setFinished] = useState(false);
 
   const params = useParams();
 
@@ -36,6 +37,9 @@ function Lesson({ setLessons, lessons }) {
   const lessonWordSlideHandler = () => {
     setIndex(index + 1);
   };
+  const finishLessonHandler = () => {
+    setFinished(true);
+  };
   const preLessonWordSlideHandler = () => {
     setIndex(index - 1);
   };
@@ -50,149 +54,51 @@ function Lesson({ setLessons, lessons }) {
 
   return (
     <>
-      <Link to="quiz">Quiz</Link>
-      <div className="lesson-cont">
-        {/* <div className="lesson-container"> */}
-        {/* {HEADER starts} */}
-        <h2 className="title-topic">{lesson.lesson}</h2>
-        {/* <div className="header-lesson"> */}
-        {/* <div className="header-img-text"> */}
-
-        {/* <div className="topic-title-and-text">
-              <h2 className="title-topic">{lesson.lesson}</h2>
-              <p>{lesson.paragraph}</p>
-            </div> */}
-        {/* {<img src={lesson.image} alt="" className="headers-img " />} */}
-
-        <div className="test-wrap">
-          <ul>
-            {index + 1 < lesson.words.length && (
-              <button onClick={lessonWordSlideHandler} className="btn-next">
-                Next
-              </button>
-            )}
-            <li>
-              <div className="word-content">
-                {lesson.words[index].genderType}
-                <img
-                  src={lesson.words[index].image}
-                  alt=""
-                  className="img-words"
-                />
-                <div className="english-word">
-                  {lesson.words[index].englishWord}
-                </div>
-              </div>
-              <div className="arabic-word">
-                {lesson.words[index].arabicWord}
-              </div>
-            </li>
-            {index > 0 && (
-              <button
-                onClick={preLessonWordSlideHandler}
-                className="btn-previous"
-              >
-                prev
-              </button>
-            )}
-          </ul>
-
-          {/* </div> */}
+      {finished && (
+        <Link to="quiz">
+          <button>Quiz</button>
+        </Link>
+      )}
+      <h2 className="title-topic">{lesson.lesson}</h2>
+      <p className="lesson-paragraph">{lesson.paragraph}</p>
+      <div className="lesson-carousel">
+        <img src={lesson.words[index].image} alt="" className="lesson-image" />
+        <div className=" lesson-text">
+          <p className=" arabic-word ">{lesson.words[index].arabicWord}</p>
+          <p className=" english-word">{lesson.words[index].englishWord}</p>
+          <p className="genderType">{lesson.words[index].genderType}</p>
         </div>
+        {index + 1 < lesson.words.length && (
+          <button
+            onClick={lessonWordSlideHandler}
+            className=" testimonial-btn btn-right"
+          >
+            Next
+          </button>
+        )}
+        {index + 1 === lesson.words.length && (
+          <button
+            onClick={finishLessonHandler}
+            className="lesson-btn btn-right"
+          >
+            END
+          </button>
+        )}
+        {index > 0 && (
+          <button
+            onClick={preLessonWordSlideHandler}
+            className=" testimonial-btn btn-left"
+          >
+            prev
+          </button>
+        )}
 
-        {/* </div> */}
-        {/* {HEADER ends} */}
-        {/* <div className="main">
-          <div className="lesson-content">
-            <div className="ul-words"> */}
-        {/* {index > 0 && (
-                <button
-                  onClick={preLessonWordSlideHandler}
-                  className="btn-previous"
-                >
-                  prev
-                </button>
-              )}
-               {index + 1 < feminine.length && (
-                <button onClick={lessonWordSlideHandler} className="btn-next">
-                  Next
-                </button>
-              )} */}
-        {/* Masculine Words */}
-        {/* <ul>
-                <div>
-                  <li>
-                    <div className="word-content">
-                      <div className="english-word">
-                        {masculine[index].englishWord}
-                      </div>
-                      <img
-                        src={masculine[index].image}
-                        alt=""
-                        className="img-words"
-                      />
-
-                      <div className="arabic-word">
-                        {masculine[index].arabicWord}
-                      </div>
-                    </div>
-                  </li>
-                </div>
-              </ul> */}
-        {/* Feminine Words */}
-        {/* {
-                <ul>
-                  <div>
-                    <li>
-                      <div className="word-content">
-                        <div className="english-word">
-                          {feminine[index].englishWord}
-                        </div>
-                        <img
-                          src={feminine[index].image}
-                          alt=""
-                          className="img-words"
-                        />
-
-                        <div className="arabic-word">
-                          {feminine[index].arabicWord}
-                        </div>
-                      </div>
-                    </li>
-                  </div>
-                </ul>
-              } */}
-        {/* {index + 1 < feminine.length && (
-                <button onClick={lessonWordSlideHandler} className="btn-next">
-                  Next
-                </button>
-              )} */}
-        {/* </div>
-          </div>
-        </div>{" "} */}
-        {/* <div className="sidebar">
-          <h2>Lessons</h2>{" "}
-          <ul>
-            {" "}
-            {lessons.map((currentLesson) => {
-              if (currentLesson.id === lesson.id) {
-                return <></>;
-              }
-              return (
-                <li key={currentLesson.id}>
-                  <div className="list-of-lessons">
-                    {currentLesson.lesson}
-                    <Link to={`/lesson/${currentLesson.id}`}>
-                      <button>Start</button>
-                    </Link>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div> */}
-        {/* <div className="footer">Footer</div> */}
-        {/* </div> */}
+        <div className="lesson-dots">
+          <button className="lesson-dot dot-fill"> </button>
+          <button className="lesson-dot"> </button>
+          <button className="lesson-dot"> </button>
+          <button className="lesson-dot"> </button>
+        </div>
       </div>
     </>
   );
